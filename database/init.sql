3457aa8588df4c8a3697a379204cadc9a4733abb
Hiing77 <supiii77@hotmail.com>
2026-09-04 09:10:56 +0200

    Ajout categorie et quantité

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,5 @@
-CREATE TABLE produit(
-    id_produit INT PRIMARY KEY,
-    nom VARCHAR(50)
-);
-
-INSERT INTO produit(id_produit, nom) 
-VALUES 
-(1, 'Clavier'),
-(2, 'Souris'),
-(3, 'Ecran');
+INSERT INTO produit (id_produit, nom, categorie, quantite) VALUES 
+(1, 'Clavier', 'Accessoire', 10),
+(2, 'Souris', 'Accessoire', 25),
+(3, 'Écran', 'Matériel', 5),
+(4, 'PC Portable', 'Matériel', 3);
